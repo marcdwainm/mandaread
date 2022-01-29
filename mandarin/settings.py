0ed1041ca1629b94993f572e34995a3dcd695955
Marc Dwain Magracia <marcdwainm@gmail.com>
2022-01-29 22:33:07 +0800

    Update Settings

--- mandarin/settings.py
+++ mandarin/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x!zw=2%bb3a30!96)^m$0_j9obje$i(#ma-ie=e=qu=egh-quy'
+# SECRET_KEY = 'django-insecure-x!zw=2%bb3a30!96)^m$0_j9obje$i(#ma-ie=e=qu=egh-quy'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mandaread.herokuapp.com']
 
 
 # Application definition
